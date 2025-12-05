2c86cf8cc58a418708dc6f64a6d297f1c01733f1
Hazem Adel <hadel6464@gmail.com>
2025-12-05 14:54:11 +0200

    writefile

--- fs_module.js
+++ fs_module.js
@@ -1,13 +1,39 @@
 const fs = require ("fs"); 
 
+
 function mkdir(folder) {
     return fs.mkdir(folder ,((err)=>{
         if(err) {
             console.log('its err')
         }else{
-            console.log('succes')
+            console.log('success')
         }
     }))
 }
 
 mkdir('folder')
+
+function remove(file) {
+    return fs.unlink(file , ((err)=>{
+        if(err) {
+            console.log('its err') 
+        }else{
+            console.log('success')
+        }
+    }))
+}
+
+remove('note.txt')
+
+function write(file) {
+    return fs.writeFile('file.txt' , file, ((err)=>{
+        if(err) {
+            console.log('err')
+        }else{
+            console.log('success')
+        }
+    }))
+}
+
+write('"Hi world"')
+
