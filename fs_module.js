50094ccb4bd5ea80d453a6507133f99f0eca9e17
Hazem Adel <hadel6464@gmail.com>
2025-12-05 14:39:00 +0200

    fs_module

--- fs_module.js
+++ fs_module.js
@@ -1,1 +1,13 @@
-git 
+const fs = require ("fs"); 
+
+function mkdir(folder) {
+    return fs.mkdir(folder ,((err)=>{
+        if(err) {
+            console.log('its err')
+        }else{
+            console.log('succes')
+        }
+    }))
+}
+
+mkdir('folder')
